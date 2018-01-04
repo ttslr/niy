2cab9d36af1031dfe354a8ac009ce9872339932c
microic <1165397725@qq.com>
2018-01-04 10:45:46 +0800

    create

--- get_sample.py
+++ get_sample.py
@@ -35,11 +35,11 @@ def add_data_batch(path):
 		im = (im - im.min()) / (im.max() - im.min()) - 0.5
 		sample_in_bytes += im.tobytes()
 
-add_data_batch('src/data_batch_1')
-add_data_batch('src/data_batch_2')
-add_data_batch('src/data_batch_3')
-add_data_batch('src/data_batch_4')
-add_data_batch('src/data_batch_5')
+add_data_batch('files/data_batch_1')
+add_data_batch('files/data_batch_2')
+add_data_batch('files/data_batch_3')
+add_data_batch('files/data_batch_4')
+add_data_batch('files/data_batch_5')
 
 with open("train_in.smpl", "wb") as file:
 	file.write(sample_in_bytes)
@@ -56,7 +56,7 @@ sample_out_head = np.zeros(8).astype('int32')
 sample_out_head[1:4] = [1, 1, 1]
 sample_out_bytes += sample_out_head.tobytes()
 
-add_data_batch('src/test_batch')
+add_data_batch('files/test_batch')
 
 with open("test_in.smpl", "wb") as file:
 	file.write(sample_in_bytes)
