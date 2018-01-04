e26200e60c77138e68bae964a8818f466cfe9e12
microic <1165397725@qq.com>
2018-01-04 10:30:46 +0800

    create

--- get_sample.py
+++ get_sample.py
@@ -4,7 +4,7 @@ import numpy as np
 import struct
 import matplotlib.pyplot as plt
 
-def get_sample_in(src, des):
+def get_sample_in(filename, des):
 	sample_in_bytes = bytearray()
 	sample_in_head = np.zeros(8).astype("int32")
 
@@ -12,7 +12,7 @@ def get_sample_in(src, des):
 
 	sample_in_bytes += sample_in_head.tobytes()
 
-	with gzip.open(src , 'rb') as file:
+	with gzip.open(filename , 'rb') as file:
 		buf = file.read()
 	 
 	index = 0
@@ -21,7 +21,7 @@ def get_sample_in(src, des):
 	magic, numImages, numRows, numColumns = struct.unpack_from(form, buf, index)
 	index += struct.calcsize(form)
 
-	print("get_sample_in", src)
+	print("get_sample_in", filename)
 	for i in range(numImages):
 		form = '>784B'
 		im_data = struct.unpack_from(form, buf, index)
@@ -36,14 +36,14 @@ def get_sample_in(src, des):
 	with open(des, "wb") as file:
 		file.write(sample_in_bytes)
 
-def get_sample_out(src, des):
+def get_sample_out(filename, des):
 	sample_out_bytes = bytearray()
 	sample_out_head = np.zeros(8).astype("int32")
 
 	sample_out_head[1:4] = [1, 1, 1]
 
 	sample_out_bytes += sample_out_head.tobytes()
-	with gzip.open(src , 'rb') as file:
+	with gzip.open(filename , 'rb') as file:
 		buf = file.read()
 
 	index = 0
@@ -51,7 +51,7 @@ def get_sample_out(src, des):
 	magic, numItems = struct.unpack_from(form, buf, index)
 	index += struct.calcsize(form)
 
-	print("get_sample_out", src)
+	print("get_sample_out", filename)
 	for i in range(numItems):
 		form = '>1B'
 		label_data = struct.unpack_from(form, buf, index)
@@ -63,8 +63,8 @@ def get_sample_out(src, des):
 	with open(des, "wb") as file:
 		file.write(sample_out_bytes)
 
-get_sample_in('src/train-images-idx3-ubyte.gz', "train_in.smpl")
-get_sample_out("src/train-labels-idx1-ubyte.gz", "train_out.smpl")
+get_sample_in('files/train-images-idx3-ubyte.gz', "train_in.smpl")
+get_sample_out("files/train-labels-idx1-ubyte.gz", "train_out.smpl")
 
-get_sample_in('src/t10k-images-idx3-ubyte.gz', "test_in.smpl")
-get_sample_out("src/t10k-labels-idx1-ubyte.gz", "test_out.smpl")
+get_sample_in('files/t10k-images-idx3-ubyte.gz', "test_in.smpl")
+get_sample_out("files/t10k-labels-idx1-ubyte.gz", "test_out.smpl")
